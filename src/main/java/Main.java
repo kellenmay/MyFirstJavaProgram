ca77e55af589139bf04eaa63260d6b17683209a2
kellenmay <kellenbmay@gmail.com>
2022-03-25 12:06:26 -0400

    swapping varibles

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,26 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("\"Hello World\"");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-
-        int x;
-        x = 123;
-        int y;
-        y = 134
+//        System.out.println("\"Hello World\"");
+//        System.out.println("Hello World");
+//        System.out.println("Hello World");
+//
+//        int x;
+//        x = 123;
+//        int y;
+//        y = 134
+//        System.out.println(x);
+//        System.out.println(x+x);
+//        System.out.println(x+y);
+        String x = "hello";
+        String y = "World";
         System.out.println(x);
-        System.out.println(x+x);
-        System.out.println(x+y);
+        System.out.println(y);
+        String temp;
+        temp = x;
+        x=y;
+        y=temp;
+        System.out.println(x);
+        System.out.println(y);
+
     }
 }
