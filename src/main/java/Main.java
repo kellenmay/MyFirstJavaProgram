5c6260653666f4f6e8bc1fb06ee67f3db7d76ca1
kellenmay <kellenbmay@gmail.com>
2022-03-25 14:03:15 -0400

    Learning logical operators

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -107,23 +107,35 @@ public class Main {
 //            System.out.println("You are NOT an Adult");
 //        }
 
-        String day = "Pizza";
-        switch(day){
-            case "Sunday": System.out.println("It is Sunday!");
-            break;
-            case "Monday": System.out.println("It is Monday!");
-            break;
-            case "Tuesday": System.out.println("It is Tuesday!");
-            break;
-            case "Wednesday": System.out.println("It is Wednesday!");
-            break;
-            case "Thursday": System.out.println("It is Thursday!");
-            break;
-            case "Friday": System.out.println("It is Friday!");
-            break;
-            case "Saturday": System.out.println("It is Saturday!");
-            break;
-            default: System.out.println("This is not a day");
+//        String day = "Pizza";
+//        switch(day){
+//            case "Sunday": System.out.println("It is Sunday!");
+//            break;
+//            case "Monday": System.out.println("It is Monday!");
+//            break;
+//            case "Tuesday": System.out.println("It is Tuesday!");
+//            break;
+//            case "Wednesday": System.out.println("It is Wednesday!");
+//            break;
+//            case "Thursday": System.out.println("It is Thursday!");
+//            break;
+//            case "Friday": System.out.println("It is Friday!");
+//            break;
+//            case "Saturday": System.out.println("It is Saturday!");
+//            break;
+//            default: System.out.println("This is not a day");
+//        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("You are playing a game! Press q or Q to quit");
+        String response = scanner.next();
+
+        if(!response.equals("q") && !response.equals("Q")){
+            System.out.println("You are still playing the game *pew pew*");
+        }
+        else{
+            System.out.println("You quit the game");
         }
 
     }
