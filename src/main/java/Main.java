c6355ae8eda8296371af359fc05844cd5999c283
kellenmay <kellenbmay@gmail.com>
2022-03-25 12:49:29 -0400

    Using operators

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,25 +24,35 @@ public class Main {
 //        y=temp;
 //        System.out.println(x);
 //        System.out.println(y);
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("What is your name?");
+//        String name = scanner.nextLine();
+//
+//        System.out.println("How old are you?");
+//        int age = scanner.nextInt();
+//        scanner.nextLine();
+//
+//        System.out.println("What is your favorite food?");
+//        String food = scanner.nextLine();
+//
+//
+//
+//
+//
+//        System.out.println("Hello "+name);
+//        System.out.println("You are "+age+" years old");
+//        System.out.println("You enjoy eating "+food);
 
-        System.out.println("What is your name?");
-        String name = scanner.nextLine();
+        double friends = 10;
+        friends--;
+        System.out.println(friends);
+        friends = (double) friends % 3;
+        System.out.println(friends);
+        friends++;
+        friends = friends % 3;
+        System.out.println(friends);
 
-        System.out.println("How old are you?");
-        int age = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.println("What is your favorite food?");
-        String food = scanner.nextLine();
-
-
-
-
-
-        System.out.println("Hello "+name);
-        System.out.println("You are "+age+" years old");
-        System.out.println("You enjoy eating "+food);
 
     }
 }
